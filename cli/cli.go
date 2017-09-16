df1b320e9fbfd695dd0afdb000bbca00287602bb
Simon Tucker <brotherlogic@gmail.com>
2017-09-16 08:01:18 -0700

    List now gets everything.

--- cli/cli.go
+++ cli/cli.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -14,7 +16,7 @@ import (
 )
 
 func findServer(name string) (string, int) {
-	conn, err := grpc.Dial("192.168.86.64:50055", grpc.WithInsecure())
+	conn, err := grpc.Dial("192.168.86.42:50055", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Cannot reach discover server: %v (trying to discover %v)", err, name)
 	}
@@ -46,16 +48,35 @@ func main() {
 	} else {
 		switch os.Args[1] {
 		case "list":
-			_, err = client.AddTaskList(context.Background(), &pb.TaskList{Name: "Testing", Tasks: &pb.ReminderList{Reminders: []*pb.Reminder{&pb.Reminder{Text: "This is task one"}, &pb.Reminder{Text: "This is task two"}}}})
+			rs, err := client.ListReminders(context.Background(), &pb.Empty{})
 			if err != nil {
 				log.Fatalf("Error adding task list %v", err)
 			}
+			fmt.Printf("%v", rs)
 		case "add":
 			reminder := os.Args[2]
 			day := os.Args[3]
 			_, err = client.AddReminder(context.Background(), &pb.Reminder{Text: reminder, DayOfWeek: day})
 			if err != nil {
 				log.Fatalf("Unable to add reminder: %v", err)
+			}
+		case "file":
+			list := &pb.ReminderList{Reminders: make([]*pb.Reminder, 0)}
+			file, err := os.Open(os.Args[2])
+			if err != nil {
+				log.Fatalf("Error reading file: %v", err)
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				elems := strings.Split(scanner.Text(), "~")
+				list.Reminders = append(list.Reminders, &pb.Reminder{Text: elems[0], GithubComponent: elems[1]})
+			}
+
+			_, err = client.AddTaskList(context.Background(), &pb.TaskList{Name: os.Args[2], Tasks: list})
+			if err != nil {
+				log.Fatalf("Error adding tasks: %v", err)
 			}
 		}
 	}
